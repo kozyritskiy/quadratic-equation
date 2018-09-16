129e6893f4e409293e4fc2f8de84391fc0edb0f7
Max Kozyritsky <maxim.kozyricki@gmail.com>
2018-09-16 20:27:05 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let parser = equation.split(' ');
+
+  let coefA = +parser[0],
+      coefB = +(parser[3] + parser[4]),
+      coefC = +(parser[7] + parser[8]);
+
+  let discriminant = Math.pow(coefB, 2) - 4 * coefA * coefC;
+
+  let rootOne = Math.round((-coefB + Math.sqrt(discriminant)) / (2 * coefA)),
+      rootTwo = Math.round((-coefB - Math.sqrt(discriminant)) / (2 * coefA));
+
+  let roots = [];
+
+  roots.push(rootOne);
+  roots.push(rootTwo);
+
+  return sortRoots = roots.sort((a, b) => a - b);
 }
